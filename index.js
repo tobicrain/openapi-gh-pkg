6d33deb61b1953d2f176af496297b68c2d21ff5e
tobicrain <tobikrain@gmail.com>
2022-11-03 19:10:37 +0100

    index js update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const actions = require("@actions/core")
-const github = require("@actions/github")
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 (
   async () => {
