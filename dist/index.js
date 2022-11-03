28cd6dd56c0f4a5a1f89397ae2968e039a00e9cd
tobicrain <tobikrain@gmail.com>
2022-11-03 22:06:28 +0100

    update

--- dist/index.js
+++ dist/index.js
@@ -59,7 +59,7 @@ const { exec } = __nccwpck_require__(2081);
         core.notice(`OpenAPI file saved to: ${openApiFile}`);
         exec('npx @openapitools/openapi-generator-cli generate -i openapi.yaml -g kotlin-spring -o kotlin --git-user-id "tandamo" --git-repo-id "scanq-client-api" --additional-properties=delegatePattern=true,apiPackage=de.scanq.client-api,artifactId=scanq-client-api,basePackage=de.scanq,artifactVersion=0.1.15,packageName=de.scanq,title=scanq-client-api');
         // list files in current directory
-        exec('ls', (err, stdout, stderr) => {
+        exec(`cd ${__dirname}; ls`, (err, stdout, stderr) => {
             if (err) {
                 //some err occurred
                 console.error(err);
