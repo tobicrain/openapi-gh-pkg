7dcd47fa3c574d0d5753a14e273b98ae0b3269c0
tobicrain <tobikrain@gmail.com>
2022-11-04 21:45:47 +0100

    update

--- dist/index.js
+++ dist/index.js
@@ -106,8 +106,9 @@ function readDir() {
         fs.writeFileSync(openApiFile, fileContent);
         core.notice(`OpenAPI file saved to: ${openApiFile}`);
         const dottedArtifactId = repoName.replace(/-/g, '.');
-        console.log(`npx @openapitools/openapi-generator-cli generate -i ${openApiFile} -g kotlin-spring -o ${__dirname}/kotlin --git-user-id "${owner}" --git-repo-id "${repoName}" --additional-properties=delegatePattern=true,apiPackage=de.${dottedArtifactId},artifactId=${repoName},basePackage=de.${dottedArtifactId.split("-")[0]},artifactVersion=${version},packageName=de.${repoName.split("-")[0]},title=${repoName}`);
-        const { stdout, stderr } = yield exec(`npx @openapitools/openapi-generator-cli generate -i ${openApiFile} -g kotlin-spring -o ${__dirname}/kotlin --git-user-id "${owner}" --git-repo-id "${repoName}" --additional-properties=delegatePattern=true,apiPackage=de.${dottedArtifactId},artifactId=${repoName},basePackage=de.${dottedArtifactId},artifactVersion=${version},packageName=de.${repoName.split("-")[0]},title=${repoName}`);
+        const firstArtifactId = dottedArtifactId.split('.')[0];
+        console.log(`npx @openapitools/openapi-generator-cli generate -i ${openApiFile} -g kotlin-spring -o ${__dirname}/kotlin --git-user-id "${owner}" --git-repo-id "${repoName}" --additional-properties=delegatePattern=true,apiPackage=de.${dottedArtifactId},artifactId=${repoName},basePackage=de.${firstArtifactId},artifactVersion=${version},packageName=de.${firstArtifactId},title=${repoName}`);
+        const { stdout, stderr } = yield exec(`npx @openapitools/openapi-generator-cli generate -i ${openApiFile} -g kotlin-spring -o ${__dirname}/kotlin --git-user-id "${owner}" --git-repo-id "${repoName}" --additional-properties=delegatePattern=true,apiPackage=de.${dottedArtifactId},artifactId=${repoName},basePackage=de.${firstArtifactId},artifactVersion=${version},packageName=de.${firstArtifactId},title=${repoName}`);
         console.log('stdout:', stdout);
         console.log('stderr:', stderr);
         // exec(, (_error: any, _stdout: any, _stderr: any) => {
