90878304c5ce13ecc02601bfb981cebd69c2cbe4
tobicrain <tobikrain@gmail.com>
2022-11-05 10:30:45 +0100

    update

--- dist/index.js
+++ dist/index.js
@@ -48,9 +48,7 @@ const fs = __importStar(__nccwpck_require__(7147));
 const constants_1 = __importDefault(__nccwpck_require__(4434));
 const { exec } = __nccwpck_require__(2081);
 const yaml = __nccwpck_require__(1917);
-const repoName = github.context.repo.repo;
-const owner = github.context.repo.owner;
-const distributionManagement = `
+const distributionManagement = (owner, repoName) => `
     <distributionManagement>
         <repository>
             <id>github</id>
@@ -73,9 +71,9 @@ function execute(command) {
         });
     });
 }
-const properties = (/* unused pure expression or super */ null && (`
+const properties = `
     <spring-boot.repackage.skip>true</spring-boot.repackage.skip>
-  </properties>`));
+  </properties>`;
 function readDir() {
     exec(`ls`, (err, stdout, stderr) => {
         if (err) {
@@ -103,49 +101,44 @@ function readDir() {
 }
 (() => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        core.notice("github.context.repo" + repoName);
+        // Set all Input Parameters
+        const githubUsername = core.getInput(constants_1.default.GITHUB_USERNAME);
+        const githubToken = core.getInput(constants_1.default.GITHUB_TOKEN);
         const openApiPath = core.getInput(constants_1.default.OPEN_API_FILE_PATH);
-        core.notice(`OpenAPI file path: ${openApiPath}`);
-        const fileContent = yield fs.promises.readFile(openApiPath, 'utf8');
-        const doc = yaml.load(fileContent);
-        const version = doc.info.version;
-        core.notice(`OpenAPI version: ${version}`);
+        const repoName = github.context.repo.repo;
+        const ownerName = github.context.repo.owner;
         const dottedArtifactId = repoName.replace(/-/g, '.');
         const firstArtifactId = dottedArtifactId.split('.')[0];
-        yield execute(`npx @openapitools/openapi-generator-cli generate -i ${openApiPath} -g kotlin-spring -o kotlin --git-user-id "${owner}" --git-repo-id "${repoName}" --additional-properties=delegatePattern=true,apiPackage=de.${dottedArtifactId},artifactId=${repoName},basePackage=de.${firstArtifactId},artifactVersion=${version},packageName=de.${firstArtifactId},title=${repoName}`);
+        const ymlFile = yield fs.promises.readFile(openApiPath, 'utf8');
+        const yml = yaml.load(ymlFile);
+        const version = yml.info.version;
+        core.notice("Repository name: " + repoName);
+        core.notice("OpenAPI file path:" + openApiPath);
+        core.notice("OpenAPI version:" + version);
+        yield execute(`npx @openapitools/openapi-generator-cli generate -i ${openApiPath} -g kotlin-spring -o kotlin --git-user-id "${ownerName}" --git-repo-id "${repoName}" --additional-properties=delegatePattern=true,apiPackage=de.${dottedArtifactId},artifactId=${repoName},basePackage=de.${firstArtifactId},artifactVersion=${version},packageName=de.${firstArtifactId},title=${repoName}`);
         core.notice(`Generated Kotlin Spring code`);
-        const data = yield fs.promises.readFile('kotlin/pom.xml', 'utf8');
-        console.log(data);
-        // exec(, (_error: any, _stdout: any, _stderr: any) => {
-        //   fs.readFile(__dirname + '/kotlin/pom.xml', 'utf8', function (err, data) {
-        //     if (err) {
-        //       return console.log(err);
-        //     }
-        //     const newData = data
-        //       .replace("</project>", distributionManagement)
-        //       .replace("</properties>", properties)
-        //     console.log(newData);
-        //     fs.writeFile(__dirname + '/kotlin/pom.xml', newData, 'utf8', function (err) {
-        //       if (err) return console.log(err);
-        //       console.log('pom.xml updated');
-        //       const GITHUB_USERNAME = core.getInput(Constants.GITHUB_USERNAME);
-        //       const GITHUB_TOKEN = core.getInput(Constants.GITHUB_TOKEN);
-        //       fs.writeFile(__dirname + '/settings.xml', `<settings><servers><server><id>github</id><username>${GITHUB_USERNAME}</username><password>${GITHUB_TOKEN}</password></server></servers></settings>`, 'utf8', function (err) {
-        //         if (err) return console.log(err);
-        //         console.log('settings.xml updated');
-        //         readDir();
-        //         exec(`cd ${__dirname}/kotlin; mvn deploy --settings ${__dirname}/settings.xml -DskipTests`, (error3: any, stdout3: any, stderr3: any) => {
-        //           console.log(`stdout3: ${stdout3}`);
-        //           console.log(`stderr3: ${stderr3}`);
-        //           if (error3) {
-        //             console.log(`error3: ${error3.message}`);
-        //             return;
-        //           }
-        //         });
-        //       });
-        //     });
-        //   });
-        // });
+        const pomFile = yield fs.promises.readFile('kotlin/pom.xml', 'utf8');
+        const newPomFile = pomFile
+            .replace("</project>", distributionManagement(ownerName, repoName))
+            .replace("</properties>", properties);
+        console.log(newPomFile);
+        core.notice(`Updated pom.xml`);
+        yield fs.promises.writeFile('kotlin/pom.xml', newPomFile, 'utf8');
+        core.notice(`Updated pom.xml`);
+        fs.writeFile(__dirname + '/settings.xml', `<settings><servers><server><id>github</id><username>${githubUsername}</username><password>${githubToken}</password></server></servers></settings>`, 'utf8', function (err) {
+            if (err)
+                return console.log(err);
+            console.log('settings.xml updated');
+            readDir();
+            exec(`cd ${__dirname}/kotlin; mvn deploy --settings ${__dirname}/settings.xml -DskipTests`, (error3, stdout3, stderr3) => {
+                console.log(`stdout3: ${stdout3}`);
+                console.log(`stderr3: ${stderr3}`);
+                if (error3) {
+                    console.log(`error3: ${error3.message}`);
+                    return;
+                }
+            });
+        });
     }
     catch (error) {
         console.error(error);
--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,8 @@ import Constants from "./util/constants";
 const { exec } = require('child_process');
 const yaml = require('js-yaml');
 
-const repoName = github.context.repo.repo as string;
-const owner = github.context.repo.owner as string;
 
-const distributionManagement = `
+const distributionManagement = (owner: string, repoName: string) => `
     <distributionManagement>
         <repository>
             <id>github</id>
@@ -64,58 +62,56 @@ function readDir() {
 
 (async () => {
   try {
-    core.notice("github.context.repo"+ repoName);
-    
+    // Set all Input Parameters
+    const githubUsername = core.getInput(Constants.GITHUB_USERNAME);
+    const githubToken = core.getInput(Constants.GITHUB_TOKEN);
     const openApiPath = core.getInput(Constants.OPEN_API_FILE_PATH);
-    core.notice(`OpenAPI file path: ${openApiPath}`);
-    
-    const fileContent = await fs.promises.readFile(openApiPath, 'utf8')
 
-    const doc = yaml.load(fileContent);
-    
-    const version = doc.info.version;
-    core.notice(`OpenAPI version: ${version}`);
-    
+    const repoName = github.context.repo.repo as string;
+    const ownerName = github.context.repo.owner as string;
+
     const dottedArtifactId = repoName.replace(/-/g, '.');
     const firstArtifactId = dottedArtifactId.split('.')[0];
 
-    await execute(`npx @openapitools/openapi-generator-cli generate -i ${openApiPath} -g kotlin-spring -o kotlin --git-user-id "${owner}" --git-repo-id "${repoName}" --additional-properties=delegatePattern=true,apiPackage=de.${dottedArtifactId},artifactId=${repoName},basePackage=de.${firstArtifactId},artifactVersion=${version},packageName=de.${firstArtifactId},title=${repoName}`);
+    const ymlFile = await fs.promises.readFile(openApiPath, 'utf8')
+    const yml = yaml.load(ymlFile);
+
+    const version = yml.info.version;
+
+    core.notice("Repository name: "+ repoName);
+    core.notice("OpenAPI file path:"+ openApiPath);
+    core.notice("OpenAPI version:"+ version);
+    
+    await execute(`npx @openapitools/openapi-generator-cli generate -i ${openApiPath} -g kotlin-spring -o kotlin --git-user-id "${ownerName}" --git-repo-id "${repoName}" --additional-properties=delegatePattern=true,apiPackage=de.${dottedArtifactId},artifactId=${repoName},basePackage=de.${firstArtifactId},artifactVersion=${version},packageName=de.${firstArtifactId},title=${repoName}`);
     core.notice(`Generated Kotlin Spring code`);
 
-    const data = await fs.promises.readFile('kotlin/pom.xml', 'utf8')
-    console.log(data)
-    // exec(, (_error: any, _stdout: any, _stderr: any) => {
-    //   fs.readFile(__dirname + '/kotlin/pom.xml', 'utf8', function (err, data) {
-    //     if (err) {
-    //       return console.log(err);
-    //     }
-    //     const newData = data
-    //       .replace("</project>", distributionManagement)
-    //       .replace("</properties>", properties)
+    const pomFile = await fs.promises.readFile('kotlin/pom.xml', 'utf8')
 
-    //     console.log(newData);
+    const newPomFile = pomFile
+      .replace("</project>", distributionManagement(ownerName, repoName))
+      .replace("</properties>", properties)
+    console.log(newPomFile);
+    core.notice(`Updated pom.xml`);
 
-    //     fs.writeFile(__dirname + '/kotlin/pom.xml', newData, 'utf8', function (err) {
-    //       if (err) return console.log(err);
-    //       console.log('pom.xml updated');
-    //       const GITHUB_USERNAME = core.getInput(Constants.GITHUB_USERNAME);
-    //       const GITHUB_TOKEN = core.getInput(Constants.GITHUB_TOKEN);
-    //       fs.writeFile(__dirname + '/settings.xml', `<settings><servers><server><id>github</id><username>${GITHUB_USERNAME}</username><password>${GITHUB_TOKEN}</password></server></servers></settings>`, 'utf8', function (err) {
-    //         if (err) return console.log(err);
-    //         console.log('settings.xml updated');
-    //         readDir();
-    //         exec(`cd ${__dirname}/kotlin; mvn deploy --settings ${__dirname}/settings.xml -DskipTests`, (error3: any, stdout3: any, stderr3: any) => {
-    //           console.log(`stdout3: ${stdout3}`);
-    //           console.log(`stderr3: ${stderr3}`);
-    //           if (error3) {
-    //             console.log(`error3: ${error3.message}`);
-    //             return;
-    //           }
-    //         });
-    //       });
-    //     });
-    //   });
-    // });
+    await fs.promises.writeFile('kotlin/pom.xml', newPomFile, 'utf8');
+    core.notice(`Updated pom.xml`);
+
+    fs.writeFile(__dirname + '/settings.xml', `<settings><servers><server><id>github</id><username>${githubUsername}</username><password>${githubToken}</password></server></servers></settings>`, 'utf8', function (err) {
+      if (err) return console.log(err);
+      console.log('settings.xml updated');
+      readDir();
+      exec(`cd ${__dirname}/kotlin; mvn deploy --settings ${__dirname}/settings.xml -DskipTests`, (error3: any, stdout3: any, stderr3: any) => {
+        console.log(`stdout3: ${stdout3}`);
+        console.log(`stderr3: ${stderr3}`);
+        if (error3) {
+          console.log(`error3: ${error3.message}`);
+          return;
+        }
+      });
+
+
+
+    });
   } catch (error) {
     console.error(error);
     core.error(JSON.stringify(error));
