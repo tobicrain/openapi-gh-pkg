0cefb8b77540f8d22783ce8cb3646a9a0b08254e
tobicrain <tobikrain@gmail.com>
2022-11-05 10:18:21 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -79,44 +79,43 @@ function readDir() {
     const dottedArtifactId = repoName.replace(/-/g, '.');
     const firstArtifactId = dottedArtifactId.split('.')[0];
 
-    console.log(`npx @openapitools/openapi-generator-cli generate -i ${openApiPath} -g kotlin-spring -o kotlin --git-user-id "${owner}" --git-repo-id "${repoName}" --additional-properties=delegatePattern=true,apiPackage=de.${dottedArtifactId},artifactId=${repoName},basePackage=de.${firstArtifactId},artifactVersion=${version},packageName=de.${firstArtifactId},title=${repoName}`)
-    const stdout = await execute(`npx @openapitools/openapi-generator-cli generate -i ${openApiPath} -g kotlin-spring -o kotlin --git-user-id "${owner}" --git-repo-id "${repoName}" --additional-properties=delegatePattern=true,apiPackage=de.${dottedArtifactId},artifactId=${repoName},basePackage=de.${firstArtifactId},artifactVersion=${version},packageName=de.${firstArtifactId},title=${repoName}`);
+    await execute(`npx @openapitools/openapi-generator-cli generate -i ${openApiPath} -g kotlin-spring -o kotlin --git-user-id "${owner}" --git-repo-id "${repoName}" --additional-properties=delegatePattern=true,apiPackage=de.${dottedArtifactId},artifactId=${repoName},basePackage=de.${firstArtifactId},artifactVersion=${version},packageName=de.${firstArtifactId},title=${repoName}`);
+    core.notice(`Generated Kotlin Spring code`);
 
-    console.log(`stdout: ${stdout}`);
-    // const data = await fs.promises.readFile('kotlin/pom.xml', 'utf8')
-    // console.log(data)
-    // // exec(, (_error: any, _stdout: any, _stderr: any) => {
-    // //   fs.readFile(__dirname + '/kotlin/pom.xml', 'utf8', function (err, data) {
-    // //     if (err) {
-    // //       return console.log(err);
-    // //     }
-    // //     const newData = data
-    // //       .replace("</project>", distributionManagement)
-    // //       .replace("</properties>", properties)
+    const data = await fs.promises.readFile('kotlin/pom.xml', 'utf8')
+    console.log(data)
+    // exec(, (_error: any, _stdout: any, _stderr: any) => {
+    //   fs.readFile(__dirname + '/kotlin/pom.xml', 'utf8', function (err, data) {
+    //     if (err) {
+    //       return console.log(err);
+    //     }
+    //     const newData = data
+    //       .replace("</project>", distributionManagement)
+    //       .replace("</properties>", properties)
 
-    // //     console.log(newData);
+    //     console.log(newData);
 
-    // //     fs.writeFile(__dirname + '/kotlin/pom.xml', newData, 'utf8', function (err) {
-    // //       if (err) return console.log(err);
-    // //       console.log('pom.xml updated');
-    // //       const GITHUB_USERNAME = core.getInput(Constants.GITHUB_USERNAME);
-    // //       const GITHUB_TOKEN = core.getInput(Constants.GITHUB_TOKEN);
-    // //       fs.writeFile(__dirname + '/settings.xml', `<settings><servers><server><id>github</id><username>${GITHUB_USERNAME}</username><password>${GITHUB_TOKEN}</password></server></servers></settings>`, 'utf8', function (err) {
-    // //         if (err) return console.log(err);
-    // //         console.log('settings.xml updated');
-    // //         readDir();
-    // //         exec(`cd ${__dirname}/kotlin; mvn deploy --settings ${__dirname}/settings.xml -DskipTests`, (error3: any, stdout3: any, stderr3: any) => {
-    // //           console.log(`stdout3: ${stdout3}`);
-    // //           console.log(`stderr3: ${stderr3}`);
-    // //           if (error3) {
-    // //             console.log(`error3: ${error3.message}`);
-    // //             return;
-    // //           }
-    // //         });
-    // //       });
-    // //     });
-    // //   });
-    // // });
+    //     fs.writeFile(__dirname + '/kotlin/pom.xml', newData, 'utf8', function (err) {
+    //       if (err) return console.log(err);
+    //       console.log('pom.xml updated');
+    //       const GITHUB_USERNAME = core.getInput(Constants.GITHUB_USERNAME);
+    //       const GITHUB_TOKEN = core.getInput(Constants.GITHUB_TOKEN);
+    //       fs.writeFile(__dirname + '/settings.xml', `<settings><servers><server><id>github</id><username>${GITHUB_USERNAME}</username><password>${GITHUB_TOKEN}</password></server></servers></settings>`, 'utf8', function (err) {
+    //         if (err) return console.log(err);
+    //         console.log('settings.xml updated');
+    //         readDir();
+    //         exec(`cd ${__dirname}/kotlin; mvn deploy --settings ${__dirname}/settings.xml -DskipTests`, (error3: any, stdout3: any, stderr3: any) => {
+    //           console.log(`stdout3: ${stdout3}`);
+    //           console.log(`stderr3: ${stderr3}`);
+    //           if (error3) {
+    //             console.log(`error3: ${error3.message}`);
+    //             return;
+    //           }
+    //         });
+    //       });
+    //     });
+    //   });
+    // });
   } catch (error) {
     console.error(error);
     core.error(JSON.stringify(error));
